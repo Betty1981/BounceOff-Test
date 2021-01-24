11f9c39abe2e529b7bad784f757ee97cc6c1d207
Betty1981 <75030703+Betty1981@users.noreply.github.com>
2021-01-24 18:11:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var a,b,car,wall,edg;
+function setup() {
+  createCanvas(400,400);
+
+  a=createSprite(400, 200, 30, 30);
+  a.shapeColor="red";
+  a.velocityX=-2;
+  //a.velocityY=-2;
+  a.debug="true";
+
+  b=createSprite(200,300,50,50);
+  b.shapeColor="red";
+  b.debug="true";
+  b.velocityY=2;
+  b.velocityX=2;
+  car=createSprite(30,200,20,20);
+  car.shapeColor="green";
+  car.velocityX=-3;
+  //car.velocityY=3;
+
+  wall=createSprite(300,200,20,100);
+  wall.shapeColor="brown";
+
+  edg=createEdgeSprites();
+}
+
+function draw() {
+  background(0); 
+  //a.x=World.mouseX;
+ // a.y=World.mouseY; 
+ 
+  /*if(isTouching(car,a)){
+    textSize(40);
+   //text("Touched",100,200);
+   car.setVelocity(0,0);
+   a.shapeColor="violet";
+   car.shapeColor="aqua";
+  }else{
+    text("Not Touched",200,200);
+  }*/
+  bouncing(a,car);
+  bouncing(a,b);
+ // bouncing(car,b);
+a.bounceOff(edg);
+car.bounceOff(edg);
+b.bounceOff(edg);
+   
+  drawSprites();
+}
+
+  
